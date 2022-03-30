4fad9b3e6d9df09d04ebca22b41061981f5d3f9d
BramB123 <23478245+BramB123@users.noreply.github.com>
2022-03-30 20:17:27 +0200

    Add UI layout

--- src/main/java/com/stendenstudenten/unogame/GameApplication.java
+++ src/main/java/com/stendenstudenten/unogame/GameApplication.java
@@ -10,19 +10,22 @@ import java.io.IOException;
 public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("card.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("playingField.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setWidth(1024);
+        stage.setHeight(768);
+        stage.setResizable(false);
         stage.show();
 
         // write your code here
-        Game game = new Game();
+        /*Game game = new Game();
         game.addPlayer("player1");
         game.addPlayer("player2");
         game.addPlayer("player3");
         game.addPlayer("player4");
-        game.startGame();
+        game.startGame();*/
     }
 
     public static void main(String[] args) {
